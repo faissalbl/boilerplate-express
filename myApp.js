1cf3b675444998f64baba937af84eae67d3faa57
Replit user <>
2023-06-07 13:42:07 -0300

    training code

--- myApp.js
+++ myApp.js
@@ -1,5 +1,53 @@
 let express = require('express');
 let app = express();
+const bodyParser = require('body-parser');
+
+const static = express.static(__dirname + '/public');
+
+
+
+app.use('/public', static);
+
+app.use((req, res, next) => {
+  console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+  next();
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res) => {
+  const indexPath = __dirname + '/views/index.html';
+  res.sendFile(indexPath);
+});
+
+app.get('/json', (req, res) => {
+  let message = 'Hello json';
+  if ('uppercase' === process.env.MESSAGE_STYLE) {
+    message = message.toUpperCase();
+  }
+  res.json({message: message});
+});
+
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString();  
+  next();
+}, (req, res) => {
+  res.json({ time: req.time });
+});
+
+app.get('/:word/echo', (req, res) => {
+  res.json({ echo: req.params.word });
+});
+
+app.route(
+  '/name'
+).get((req, res) => {
+  const { first, last } = req.query;
+  res.json({ name: `${first} ${last}` });
+}).post((req, res) => {
+  const { first, last } = req.body;
+  res.json({ name: `${first} ${last}` });
+});
 
 
 
@@ -31,9 +79,4 @@ let app = express();
 
 
 
-
-
-
-
-
- module.exports = app;
+module.exports = app;
